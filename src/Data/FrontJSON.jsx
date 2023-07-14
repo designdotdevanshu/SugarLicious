c56f9f48ec800a820d5b7ef74d8607f6ba9efa7b
ShubhamJoshii <shubhamjoshii676@gmail.com>
2023-07-14 21:54:40 +0530

    Added Data

---
+++ src/Data/FrontJSON.jsx
@@ -0,0 +1,152 @@
+import Image1 from "./assets/Frontpage/Image (10).png"
+import Image2 from "./assets/Frontpage/Image (13).png"
+import Image3 from "./assets/Frontpage/Image (16).png"
+import Image4 from "./assets/Frontpage/Image (22).png"
+import Image5 from "./assets/Frontpage/Image (27).png"
+import Image6 from "./assets/Frontpage/Image (16).png"
+import Image7 from "./assets/Frontpage/Image (28).png"
+import Image8 from "./assets/Frontpage/Image (10).png"
+import Image9 from "./assets/Frontpage/Image (17).png"
+import Image10 from "./assets/Frontpage/Image (23).png"
+import Image11 from "./assets/Frontpage/Image (21).png"
+import Image12 from "./assets/Frontpage/Image (26).png"
+import Image13 from "./assets/Frontpage/Image (35).png"
+import Image14 from "./assets/Frontpage/Image (36).png"
+import Image15 from "./assets/Frontpage/Image (46).png"
+import Image16 from "./assets/Frontpage/Image (47).png"
+import Image17 from "./assets/Frontpage/Image (48).png"
+import Image18 from "./assets/Frontpage/Image (49).png"
+import Image19 from "./assets/Frontpage/Image (50).png"
+import Image20 from "./assets/Frontpage/Image (29).png"
+import Image21 from "./assets/Frontpage/Image (30).png"
+import Image22 from "./assets/Frontpage/Image (31).png" 
+import Image23 from "./assets/Frontpage/Image (32).png" 
+import Image24 from "./assets/Frontpage/Image (33).png" 
+
+
+// for Desktop 12
+const FrontJSON = [ 
+    {
+        Title:"OUR SPECIALITY",
+        Products:[
+            {
+                Name:"LATTE COFFEE",
+                Image:Image1
+            },  
+            {
+                Name:"CHOCOLATE MUFFIN",
+                Image:Image2
+            },  
+            {
+                Name:"BUTTERSCOTCH MUFFIN",
+                Image:Image3
+            },  
+            {
+                Name:"MOJITO",
+                Image:Image4
+            },  
+            {
+                Name:"DONUTS",
+                Image:Image5
+            }  
+        ]
+    },
+    {
+        Title:"OUR  PRODUCTS",
+        Products:[
+            {
+                Name:"MUFFIN AND CAKE",
+                Image:Image6
+            },  
+            {
+                Name:"PIZZA",
+                Image:Image7
+            },  
+            {
+                Name:"COFFEE",
+                Image:Image8
+            },  
+            {
+                Name:"JUICE",
+                Image:Image9
+            },  
+            {
+                Name:"SMOOTHIE",
+                Image:Image10
+            },  
+            {
+                Name:"ICE CREAM",
+                Image:Image20
+            } , 
+            {
+                Name:"SOFT DRINK",
+                Image:Image21
+            } , 
+            {
+                Name:"NOODLES",
+                Image:Image22
+            } , 
+            {
+                Name:"BURGER",
+                Image:Image23
+            } , 
+            {
+                Name:"FRENCH FRIES",
+                Image:Image24
+            }  
+        ]
+    },
+    {
+        Title:"COMBOS",
+        Products:[
+            {
+                Name:"BUY 1 GET 1 FREE",
+                Image:Image11
+            },  
+            {
+                Name:"BURGUR",
+                Image:Image12
+            },  
+            {
+                Name:"DONUTS",
+                Image:Image13
+            },  
+            {
+                Name:"COFFEE",
+                Image:Image14
+            }  
+        ]
+    }
+]
+
+const Reviews=
+[
+    {
+        CustomerName:"Kavita Mehra",
+        Review:"Ambience of the cafe is so good & interior design of the cafe was appealing and perhaps had a unique or stylish vibe./n Particularly, Lattee Coffee is my favourite.",
+        CustomerImage:Image15
+    },
+    {
+        CustomerName:"Shubham joshi",
+        Review:"I have booked seats in the PERKY BEANS to celebrate my birthday with my family. Everything is per the plan./n I enjoy my special day with nice and cool ambience.",
+        CustomerImage:Image16
+    },
+    {
+        CustomerName:"Himanshi joshi",
+        Review:"Quality is a crucial aspect of any cafe, especially when it comes to serving drinks and sweets. Customers appreciate cafes that prioritize high-quality ingredients, preparation methods, and overall craftsmanship in their offerings. It's important for cafes like Perky Beans to maintain consistent quality in their drinks and sweets to provide an enjoyable and satisfying experience for their customer./n Unique and Exceptional Products :Offering unique and exceptional drinks and sweets can set a cafe ",
+        CustomerImage:Image17
+    },
+    {
+        CustomerName:"Aman Sharma",
+        Review:"Quality is a crucial aspect of any cafe, especially when it comes to serving drinks and sweets. Customers appreciate cafes that prioritize high-quality ingredients, preparation methods, and overall craftsmanship in their offerings. It's important for cafes like Perky Beans to maintain consistent quality in their drinks and sweets to provide an enjoyable and satisfying experience for their customer./n Unique and Exceptional Products :Offering unique and exceptional drinks and sweets can set a cafe. ",
+        CustomerImage:Image18
+    },
+    {
+        CustomerName:"Binna",
+        Review:"Quality is a crucial aspect of any cafe, especially when it comes to serving drinks and sweets. Customers appreciate cafes that prioritize high-quality ingredients, preparation methods, and overall craftsmanship in their offerings. It's important for cafes like Perky Beans to maintain consistent quality in their drinks and sweets to provide an enjoyable and satisfying experience for their customer. Unique and Exceptional Products :Offering unique and exceptional drinks and sweets can set a cafe ",
+        CustomerImage:Image19
+    }
+]
+
+export {FrontJSON,Reviews};
+
