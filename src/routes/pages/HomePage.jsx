1a2c99b31c2dbebd4d08cf31f82bff9a28a11726
Devanshu <design.devanshu@gmail.com>
2023-07-15 18:33:47 +0530

    Removed unnecessary code

--- src/routes/pages/HomePage.jsx
+++ src/routes/pages/HomePage.jsx
@@ -1,20 +1,9 @@
 import React from "react";
-import Contact from "../../components/Landing/Contact/Contact";
-import Feature from "../../components/Landing/Feature/Feature";
-import Layout from "../../components/Layout/Layout";
-import AnimatedComponent from "../../animation/animation";
 
 const HomePage = () => {
   return (
     <React.Fragment>
-      <Layout>
-        <AnimatedComponent>
-          <Feature />
-        </AnimatedComponent>
-        <AnimatedComponent>
-          <Contact />
-        </AnimatedComponent>
-      </Layout>
+      <h1>Hello</h1>
     </React.Fragment>
   );
 };
