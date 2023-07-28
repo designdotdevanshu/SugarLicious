8b4388c953cdfba1394c196fa628e2a25d660426
Devanshu <design.devanshu@gmail.com>
2023-07-28 20:38:53 +0530

    scss fixed

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "../src/routes/App.jsx";
-import "./index.css";
+import "../src/styles/scss/style.scss";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
